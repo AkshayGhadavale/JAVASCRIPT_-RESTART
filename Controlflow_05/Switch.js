2c1c603cf725447b935e84894a429c2e8745bf0e
Akshay Ghadavale <52067529+AkshayGhadavale@users.noreply.github.com>
2023-11-18 13:50:30 +0000

    aksak

---
+++ Controlflow_05/Switch.js
@@ -0,0 +1,24 @@
+
+
+const month =3;
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("apr")
+        break;
+    case 5:
+        console.log("may")
+        break;
+
+    default:
+        console.log("defult")
+        break;
+}
---
+++ Itreations/one.js
@@ -0,0 +1,17 @@
+
+// for (let i = 0; i <= 10; i++) 
+// {
+//     const element =i;
+//     console.log(element  );
+    
+// } 
+
+
+for (let i = 1; i <=10; i++) {
+    for(let j= 1;j<=10;j++)
+    {
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+    console.log(" ")
+    
+}
